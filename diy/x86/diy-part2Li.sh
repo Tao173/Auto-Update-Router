b544144e8e10b9a8c5ede99450dc17e81183dfc0
Wanzi <Mr.Wanzi@outlook.com>
2022-07-22 00:06:10 +0800

    Update diy-part2Li.sh

--- diy/x86/diy-part2Li.sh
+++ diy/x86/diy-part2Li.sh
@@ -25,6 +25,8 @@ rm -rf feeds/luci/applications/luci-app-qbittorrent
 rm -rf feeds/luci/applications/luci-app-smartdns 
 
 # 添加额外软件包
+#L大插件支持库
+git clone --recursive https://github.com/op4packages/myPackages.git package/myPackages
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
